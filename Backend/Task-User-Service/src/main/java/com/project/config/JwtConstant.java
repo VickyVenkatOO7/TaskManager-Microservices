b77df810b99184736b304b13adc271cc967e5730
Trinadh Sai Rapeti <116419508+VickyVenkatOO7@users.noreply.github.com>
2024-10-31 10:48:33 +0530

    Delete Backend directory

--- Backend/Task-User-Service/src/main/java/com/project/config/JwtConstant.java
+++
@@ -1,6 +0,0 @@
-package com.project.config;
-
-public class JwtConstant {
-	public static final String SECRET_KEY = "jcjbdskjvgdiuvadsivdskvjadsgdsvvdshvdfiuvivhf";
-	public static final String JWT_HEADER = "Authorization";
-}
