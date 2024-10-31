403e7069a44018c8fa28e8005e4b624773118029
VickyVenkatOO7 <venkatsatya7101@gmail.com>
2024-10-31 11:16:30 +0530

    Initialized Task-Service

---
+++ TaskManagemnt/Backend/Task-Service/src/main/java/com/project/Controller/TaskController.java
@@ -0,0 +1,26 @@
+package com.project.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.model.Task;
+import com.project.service.TaskService;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+	
+	@Autowired
+	private TaskService taskService;
+	
+	public ResponseEntity<Task> createTask(@RequestBody Task task, @RequestHeader("Authorization") String jwt) {
+		
+		Task createdTask = taskService.createTask(task, );
+		return null;
+	}
+
+}
---
+++ TaskManagemnt/Backend/Task-Service/src/main/java/com/project/service/TaskService.java
@@ -0,0 +1,26 @@
+package com.project.service;
+
+import java.util.List;
+
+import com.project.model.Task;
+import com.project.model.TaskStatus;
+
+public interface TaskService {
+	
+	Task createTask(Task task, String requesterRole) throws Exception;
+	
+	Task getTaskById(Long id) throws Exception;
+	
+	List<Task> getAllTasks(TaskStatus status);
+	
+	Task updateTask(Long id, Task updateTask, Long userId) throws Exception;
+	
+	void deleteTask(Long id) throws Exception;
+	
+	Task assignedToUser(Long userId, Long taskId) throws Exception;
+	
+	List<Task> assignedUsersTasks (Long userId, TaskStatus status);
+	
+	Task completeTask(Long taskId) throws Exception;
+
+}
