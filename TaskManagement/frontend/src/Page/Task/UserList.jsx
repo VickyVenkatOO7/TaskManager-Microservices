409541152a16f8ecf518f40dd170feb599fa303f
VickyVenkatOO7 <venkatsatya7101@gmail.com>
2024-11-03 01:47:21 +0530

    Done with Submission and User modals

--- TaskManagement/frontend/src/Page/Task/UserList.jsx
+++ TaskManagement/frontend/src/Page/Task/UserList.jsx
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import { Avatar, Divider, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
 
 const style = {
   position: 'absolute',
@@ -11,16 +12,18 @@ const style = {
   transform: 'translate(-50%, -50%)',
   width: 400,
   bgcolor: 'background.paper',
-  border: '2px solid #000',
+  outline: "none",
   boxShadow: 24,
-  p: 4,
+  p: 2,
 };
 
-export default function UserList({handleClose, open}) {
+const tasks = [1,1,1,1]
+
+export default function UserList({ handleClose, open }) {
 
   return (
     <div>
-      
+
       <Modal
         open={open}
         onClose={handleClose}
@@ -28,9 +31,28 @@ export default function UserList({handleClose, open}) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            User List
-          </Typography>
+          {
+            tasks.map((item, index) => 
+            <>
+              <div className='flex items-center justify-between w-full'>
+              <div>
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar src='https://images3.alphacoders.com/132/thumbbig-1328396.webp' />
+                  </ListItemAvatar>
+                  <ListItemText
+                    secondary="@trinadh"
+                    primary={"Trinadh"} />
+                </ListItem>
+              </div>
+              <div>
+                <Button className='customButton'>select</Button>
+              </div>
+            </div>
+            {index !== tasks.length-1 && <Divider variant='inset'/>}
+            </>
+            )
+          }
         </Box>
       </Modal>
     </div>
